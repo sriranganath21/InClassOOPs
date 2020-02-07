59d5f02a35d44e23e63cf8e5ed1de412d29519f6
Sriranganath Shankam Manjunatha Prasanna <sriranganath@usf.edu>
2020-02-07 11:48:31 -0500

    Add project files.

---
+++ InClassOOPs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InClassOOPs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car myObj = new car();
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.maxSpeed);
+        }
+    }
+}
---
+++ InClassOOPs/car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InClassOOPs
+{
+    public class car
+
+    {
+       string color = "red";
+       int maxSpeed = 200;          
+       public void fullThrottle()
+       {
+            Console.WriteLine("The car is going as fast as it can!");
+       }
+    }
+}
